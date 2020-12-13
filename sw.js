52cbbc7e8855cfd93b38cc18c5ecc66bdf46e458
oty <oty@otys-MacBook-Pro.local>
2020-12-13 14:43:25 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,27 +27,35 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-048aad0191c252f79aa3.js"
+    "url": "webpack-runtime-3ad460df3a7bfbc8b4b2.js"
   },
   {
     "url": "framework-83e11af5cf111be5ad89.js"
   },
   {
-    "url": "app-10709a0dcc4c35d8f2a0.js"
+    "url": "app-9f26dafe6fa554f63788.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "78f8b2e6bbac7ee693891f615e76fa46"
+    "revision": "160554fb98ed0aa4f2364fe5d11e9c27"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
+  },
+  {
+    "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
+    "revision": "f6081b83111aea4128c98944b7fafccc"
+  },
+  {
+    "url": "page-data/app-data.json",
+    "revision": "b7bfbf51609d4f88d8bcac49e14dff7c"
   },
   {
     "url": "polyfill-4b1e8b7e04c413f9811f.js"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "b057b109d430adf26ffded438ed90446"
+    "revision": "938f5fc1fbda4948467f845c3ca6efef"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -134,12 +142,12 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   lastNavigationRequest = event.request.url
 
   let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^`), ``)
+  pathname = pathname.replace(new RegExp(`^/oty_company_website`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-10709a0dcc4c35d8f2a0.js`))) {
+  if (!resources || !(await caches.match(`/oty_company_website/app-9f26dafe6fa554f63788.js`))) {
     return await fetch(event.request)
   }
 
@@ -152,7 +160,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
+  const offlineShell = `/oty_company_website/offline-plugin-app-shell-fallback/index.html`
   const offlineShellWithKey = workbox.precaching.getCacheKeyForURL(offlineShell)
   return await caches.match(offlineShellWithKey)
 })
